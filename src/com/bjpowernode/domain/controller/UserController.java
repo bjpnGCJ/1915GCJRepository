e20c1fe5609ace483d6708b92f398342978232ad
bjpnGCJ <1079528193@qq.com>
2020-04-07 20:38:01 +0800

    添加了User控制类

---
+++ src/com/bjpowernode/domain/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.domain.controller;
+
+/**
+ * @Author: 管
+ * @Date: 2020/4/7 20:27
+ */
+public class UserController {
+    public static void main(String[] args) {
+        System.out.println("添加了一个控制类");
+    }
+}
